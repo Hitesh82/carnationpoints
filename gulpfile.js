d6a54a0ec740bc4d0155d4d7c2a45ead70dfd90b
hitesh82 <hitesh.gopal@gmail.com>
2025-12-29 18:32:19 +0530

    new update

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,19 @@
-const gulp = require("gulp");
+const { src, dest, series } = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const plumber = require("gulp-plumber");
 
-// JS Paths
-const jsPaths = {
-  src: ["./src/*.js"],
-  output: "./dist/",
-};
-
-// Gulp task to concatenate and minify JS
 function scripts() {
-  return (
-    gulp
-      .src(jsPaths.src, { sourcemaps: true })
-      // Combine all JS files into one
-      .pipe(concat("carnationpoints.js"))
-      // Output normal (unminified) version
-      .pipe(gulp.dest(jsPaths.output))
-      // Then minify it
-      .pipe(uglify())
-      // Rename to .min.js
-      .pipe(rename({ suffix: ".min" }))
-      // Output minified version
-      .pipe(gulp.dest(jsPaths.output))
-  );
+  return src("src/**/*.js")
+    .pipe(plumber()) // prevent crash on errors
+    .pipe(concat("carnationpoints.js")) // normal file
+    .pipe(dest("dist")) // dist/carnationpoints.js
+    .pipe(rename({ suffix: ".min" })) // rename to .min.js
+    .pipe(uglify()) // minify
+    .pipe(dest("dist")); // dist/carnationpoints.min.js
 }
 
-// Watch task
-function watchFiles() {
-  gulp.watch(jsPaths.src, scripts);
-}
-
-// Export tasks
-exports.scripts = scripts;
-exports.watchFiles = watchFiles;
-exports.default = gulp.series(scripts);
+exports.build = series(scripts);
+exports.default = series(scripts);
+  
